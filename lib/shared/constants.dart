9e8d88b69bcfa9fad914bd29f279e67f4adaae19
sujal121 <vjdeveloper121@gmail.com>
2022-09-09 15:18:57 +0530

    OK

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:1004949599178:web:a611c8d75a7ce2e9b0d533";
+  static String apiKey = "AIzaSyBBoOsdXljMF3e-2BFrzSlOsjZLPlmbAPw";
+  static String messagingSenderId = "1004949599178";
+  static String projectId = "textify1";
+  final primaryColor = const Color(0xFFee7b64);
+}
